bf5d92f4187228d2d6a7f9b61b88f2f2290d887e
Rafal.sz <rafal.sz@tutanota.com>
2021-04-17 22:27:03 +0200

    Simplified UserDetails implementation

--- src/main/java/dev/backend/UniTalk/security/services/UserDetailsImpl.java
+++ src/main/java/dev/backend/UniTalk/security/services/UserDetailsImpl.java
@@ -15,39 +15,16 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 public class UserDetailsImpl implements UserDetails {
-    private static final long serialVersionUID = 1L;
-
-    private final Long id;
-
-    private final String username;
-
-    private final String email;
-
-    @JsonIgnore
-    private final String password;
 
     private final Collection<? extends GrantedAuthority> authorities;
 
-    public UserDetailsImpl(Long id, String username, String email, String password,
-                           Collection<? extends GrantedAuthority> authorities) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.authorities = authorities;
-    }
+    private final User user;
 
-    public static UserDetailsImpl build(User user) {
-        List<GrantedAuthority> authorities = user.getRoles().stream()
+    public UserDetailsImpl(User user) {
+        this.user = user;
+        authorities = user.getRoles().stream()
                 .map(role -> new SimpleGrantedAuthority(role.getName().name()))
                 .collect(Collectors.toList());
-
-        return new UserDetailsImpl(
-                user.getId(),
-                user.getUsername(),
-                user.getEmail(),
-                user.getPassword(),
-                authorities);
     }
 
     @Override
@@ -56,21 +33,21 @@ public class UserDetailsImpl implements UserDetails {
     }
 
     public Long getId() {
-        return id;
+        return user.getId();
     }
 
     public String getEmail() {
-        return email;
+        return user.getEmail();
     }
 
     @Override
     public String getPassword() {
-        return password;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return username;
+        return user.getUsername();
     }
 
     @Override
@@ -100,6 +77,6 @@ public class UserDetailsImpl implements UserDetails {
         if (o == null || getClass() != o.getClass())
             return false;
         UserDetailsImpl user = (UserDetailsImpl) o;
-        return Objects.equals(id, user.id);
+        return Objects.equals(getId(), user.getId());
     }
 }
