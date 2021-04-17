4aaebd18d7c0af0cd7c7befef537732aa9a3e573
Rafal.sz <rafal.sz@tutanota.com>
2021-04-17 22:11:02 +0200

    Disabled global security

--- src/main/java/dev/backend/UniTalk/security/WebSecurityConfig.java
+++ src/main/java/dev/backend/UniTalk/security/WebSecurityConfig.java
@@ -67,8 +67,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/api/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
