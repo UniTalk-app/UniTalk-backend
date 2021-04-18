1feecbdab752b135235f215425e48a21b243d76e
Paweł <paweltomczyk444@gmail.com>
2021-04-18 18:05:42 +0200

    UNI-76 cleaning

--- src/main/java/dev/backend/UniTalk/exception/CategoryException.java
+++
@@ -1,9 +0,0 @@
-package dev.backend.UniTalk.exception;
-
-public class CategoryException extends RuntimeException
- {
-    public CategoryException(Long id)
-    {
-        super("Could not find category " + id);
-    }
-}
