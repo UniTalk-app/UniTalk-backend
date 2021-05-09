e6e8a236b2ccf270b425f676424ccd6c6c819bbe
Krystian Turek <krystian.turek2@gmail.com>
2021-05-09 19:19:37 +0200

    fixed user loader bug

--- src/main/java/dev/backend/UniTalk/user/UserLoader.java
+++ src/main/java/dev/backend/UniTalk/user/UserLoader.java
@@ -1,20 +1,27 @@
 package dev.backend.UniTalk.user;
 
+import dev.backend.UniTalk.role.ERole;
+import dev.backend.UniTalk.role.Role;
+import dev.backend.UniTalk.role.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class UserLoader implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     @Autowired
-    public UserLoader(UserRepository userRepository) {
+    public UserLoader(UserRepository userRepository,
+                      RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     @Autowired
@@ -24,5 +31,13 @@ public class UserLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         User user = new User("username", "firstName", "lastName", "email@email", encoder.encode("qwerty"));
         this.userRepository.save(user);
+
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+        roles.add(userRole);
+
+        user.setRoles(roles);
+        userRepository.save(user);
     }
 }
