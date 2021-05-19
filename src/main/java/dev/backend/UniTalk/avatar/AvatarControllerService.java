b36d0ef0ba5dbd0280594bbce198d83c97ae5fa5
Krystian Turek <krystian.turek2@gmail.com>
2021-05-19 11:06:23 +0200

    avatar - final

--- src/main/java/dev/backend/UniTalk/avatar/AvatarControllerService.java
+++ src/main/java/dev/backend/UniTalk/avatar/AvatarControllerService.java
@@ -1,10 +1,7 @@
 package dev.backend.UniTalk.avatar;
 
-
-
 import dev.backend.UniTalk.exception.ResourceNotFoundException;
 import dev.backend.UniTalk.user.User;
-import dev.backend.UniTalk.user.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -16,23 +13,14 @@ import java.io.IOException;
 public class AvatarControllerService {
 
     private final AvatarRepository avatarRepository;
-    private final UserRepository userRepository;
 
-    public AvatarControllerService(AvatarRepository avatarRepository,
-                                   UserRepository userRepository) {
+    public AvatarControllerService(AvatarRepository avatarRepository) {
         this.avatarRepository=avatarRepository;
-        this.userRepository=userRepository;
     }
-
-
 
 
     public byte[] getAvatar(User user) {
 
-//        final Avatar avatarFromBD = avatarRepository.findBy(user.getAvatarId())
-//                .orElseThrow(() -> new ResourceNotFoundException("Not found avatar with id = " + user.getAvatarId()));
-
-        //Avatar avatar = user.getAvatar();
         if(user.getAvatar() == null)
             throw new ResourceNotFoundException("User does not have an avatar");
 
@@ -45,26 +33,19 @@ public class AvatarControllerService {
             throw new IOException("User already has an avatar");
 
         Avatar avatar = new Avatar(imageFile.getBytes());
-
         user.setAvatar(avatar);
         avatar.setUser(user);
-
-//        avatarRepository.save(avatar);
-//        userRepository.save(user);
+        avatarRepository.save(avatar);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     public ResponseEntity<HttpStatus> updateAvatar(User user, MultipartFile imageFile) throws IOException {
 
-//        Avatar avatar = avatarRepository.findById(user.getAvatarId())
-//                .orElseThrow(() -> new ResourceNotFoundException("Not found avatar with id = " + user.getAvatarId()));
-
         if(user.getAvatar() == null)
             throw new ResourceNotFoundException("User does not have an avatar");
 
         Avatar avatar = user.getAvatar();
-
         avatar.setImage(imageFile.getBytes());
         avatarRepository.save(avatar);
 
@@ -77,6 +58,7 @@ public class AvatarControllerService {
             throw new ResourceNotFoundException("User does not have an avatar");
 
         avatarRepository.delete(user.getAvatar());
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
