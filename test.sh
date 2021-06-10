113da14445baef28ef295863f3a0af8170e21e2c
Rafal.sz <rafal.sz@tutanota.com>
2021-06-10 23:27:44 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,3 @@
+docker run -p 5432:5432 --name postgres -e POSTGRES_PASSWORD=passwd -d --rm postgres
+while ! timeout 1 bash -c "echo > /dev/tcp/localhost/5432"; do echo "Waiting for Postgres..."; sleep 1; done
+mvn test
